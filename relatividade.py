c2fb3e2ca8ead4331ed33699433bb9c8c86ce043
Cairo-Henrique <98197188+Cairo-Henrique@users.noreply.github.com>
2022-02-07 15:09:15 -0300

    Add files via upload

--- relatividade.py
+++ relatividade.py
@@ -1,5 +1,5 @@
 print('Calculadora relativística')
-grandeza = str(input('Quer calcular o espaço ou tempo? (s/t) '))
+grandeza = str(input('Quer calcular utilizar a fórmula do espaço ou do tempo? (s/t) '))
 if (grandeza == 'tempo') or (grandeza == 't'):
     escolha = str(input('O que quer calcular? (movementtime, resttime ou v) '))
     if escolha == 'movementtime':
@@ -12,8 +12,10 @@ if (grandeza == 'tempo') or (grandeza == 't'):
         v = float(input('Informe a velocidade do mesmo referencial em movimento em m/s: '))
         gama = 1/((1-(v**2/89875517873681764))**0.5)
         print("∆t =",t/gama)
-    elif escolha == 'v':
-        print("em desenvolvimento")
+    elif escolha == 'velocidade' or escolha == 'v':
+        t = float(input('Informe o tempo que passou para o referencial em repouso: '))
+        t0 = float(input('Informe o tempo que passou para o referencial em movimento: '))
+        print('V =',(-t**2/t0**2+1)**0.5*299792458)
 if (grandeza == 'espaco') or (grandeza == 'espaço') or (grandeza == 's'):
     escolha = str(input('O que quer calcular? (movementlength, restlength ou v) '))
     if escolha == 'movementlength':
@@ -26,5 +28,5 @@ if (grandeza == 'espaco') or (grandeza == 'espaço') or (grandeza == 's'):
         v = float(input('Informe a velocidade do mesmo referencial em movimento em m/s: '))
         gama = 1/((1-(v**2/89875517873681764))**0.5)
         print("L =",L*gama)
-    elif escolha == 'v':
-        print("Em desenvolvimento")
+    elif escolha == 'velocidade' or escolha == 'v':
+        print('Em desenvolvimento.')
